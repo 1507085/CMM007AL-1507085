736579d33d5bec08172f6aaddf71c1ebf9c3160c
1507085 <totious500@gmail.com>
2016-05-04 11:43:58 +0100

    a

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Landing</title>
+    <link rel="stylesheet" href="stle.css">
+    <link rel="stylesheet" href="unsemantic-grid-responsive-tablet.css">
+</head>
+<body>
+<header>
+    <h1>myBlog</h1>
+    <h2>because the internet needs to know what I think</h2>
+    <nav>
+        <ul>
+            <li><a id="aa" href="blog.php">All Blog Items</a></li>
+            <li><a id="bb" href="blog.php">Work Blog Items</a></li>
+            <li><a id="cc" href="blog.php">University Blog Items</a></li>
+            <li><a id="dd" href="blog.php">Family Blog Items</a></li>
+            <li><a id="ee" href="add.php">Insert Blog Item</a></li>
+        </ul>
+    </nav>
+</header>
+<main class="grid-container">
+    <section class="grid-66">
+        <p>Lorem ipsum dolor sit amet, consectetur<br> adipiscing elit, sed de eiusmod tempor
+            incididunt<br> ut labore et dolore magna aliqua. Ut enim ad <br>minim veniam, quis nostrud exercitation
+            ullamco<br> laboris nisi aliquip ex ea commodo consequat.<br>Duis aute irure dolor in reprehrnderit in voluptate<br>
+            velit esse cillum dolore eu fugiat nulla pariatur.<br> Excepteur sint occaecat cupidatat non proident.<br>
+            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </section>
+    <section class="grid-33">
+        <aside>
+            <img id="kk" src="blog.png" alt="blog image" width="250">
+        </aside>
+    </section>
+</main>
+<footer>
+    <p>Designed by [Emmanuel Onumah],[2016].</p>
+</footer>
+</body>
+</html>
